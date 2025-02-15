70271df5a282c1b47c261ddf5a0facd99e4be6e7
zavodtsvetov <mitfahov@gmail.com>
2025-02-15 11:39:36 +0500

    overwrite with no jsx, only js as i guess

--- React-Task-1/src/App.jsx
+++ React-Task-1/src/App.jsx
@@ -1,39 +1,47 @@
-//import & export можно считать декларативным подходом, ведь мы не знаем каким образом программа переносит информацию с одного файла на другой
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
+import { createElement } from 'react'
 
-// В функции императивный подход совмещается с декларативным. Мы вызываем отрисовку тэгов как в HTML не думая как отрисуются пиксели, но подход станет более императивным, если мы будет сreate элементы вручную
-
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
+  return (createElement(
+    'div',
+    null,
+    createElement('a', {href:"https://vite.dev", target:"_blank"}, 
+      createElement('img',
+        {src: viteLogo, className:"logo", alt:"Vite logo"}
+      )  
+  ),
+  createElement('a', {href:"https://react.dev", target:"_blank"}, 
+    createElement('img',
+      {src: reactLogo, className:"logo react", alt:"React logo"}
+    )  
+),createElement('h1',
+  null,
+  'Vite + React'
+),createElement('div',
+  {className:"card"},
+  createElement(
+    'button',
+    {onClick:() => setCount((count) => count + 1)},
+    `counts is ${count}`
+  ),
+  createElement(
+    'p',
+    null,
+    "Edit ", createElement('code',null,"src/App.jsx"), " and save to test HMR"
+  ),
+  createElement('p',
+    null,
+    `CURRENT YEAR: ${new Date().getFullYear()}`
+  ),
+  createElement('p',
+    {className: "read-the-docs"},
+    'Click on the Vite and React logos to learn more'
+  ),
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p> CURRENT YEAR: {new Date().getFullYear()}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+))
+)
 }
-
-export default App
